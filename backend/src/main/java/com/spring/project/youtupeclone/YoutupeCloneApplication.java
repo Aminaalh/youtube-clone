0c3d0baf5373fe53277484fe7e8e204c8b4db12c
Amina <alhaouil.amina@gmail.com>
2023-03-19 02:34:30 +0100

    application architecture

---
+++ backend/src/main/java/com/spring/project/youtupeclone/YoutupeCloneApplication.java
@@ -0,0 +1,13 @@
+package com.spring.project.youtupeclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutupeCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutupeCloneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/spring/project/youtupeclone/YoutupeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.project.youtupeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutupeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
